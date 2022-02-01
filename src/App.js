ecba87779acb6e028b6a74d75c9cea54c02f8934
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-01 05:47:45 -0700

    Add setSlogans state handler function to SloganForm component in App

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   // you'll need to track a city name, which starts as the city name of your choice.
   const [cityName, setCityName] = useState('Los Angeles');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
-  const [slogan, setSlogan] = useState(['Los Angeles brings it all together.', 'The City of Angels', 'City of Champions']);
+  const [slogans, setSlogans] = useState(['Los Angeles brings it all together.', 'The City of Angels', 'City of Champions']);
 
   return (
     <div className="App">
@@ -44,9 +44,9 @@ function App() {
           <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
-        <SloganForm />
+        <SloganForm setSlogan={setSlogans} />
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-        <SloganList slogan={slogan} />
+        <SloganList slogan={slogans} />
 
       </div>
     </div>
--- src/SloganForm.js
+++ src/SloganForm.js
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 
-export default function SloganForm({ setSlogans, slogans }) {
-  // React forms are a pain! 
-  
   // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans. 
-  // Also, this component also takes in a prop called slogans, which is an array of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
-  const [sloganInput, setSloganInput] = useState('');
+  // Also, this component also takes in a prop called slogans, which is an array of strings. //
+export default function SloganForm({ slogans, setSlogans }) {
   // Track the sloganInput form state with a useState hook
+  const [sloganInput, setSloganInput] = useState('');
+  
+  // On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
   function handleSubmit(e) {
     e.preventDefault();
 
     setSlogans([...slogans, sloganInput]);
-
-
     // set the sloganInput form state to an empty string to reset the form
+    setSloganInput('');
   }
 
   return (
@@ -23,8 +22,8 @@ export default function SloganForm({ setSlogans, slogans }) {
         Add a slogan to the list!
         {/* on change, set the sloganInput in state to be the input value */}
         {/* also, weirdly, set the value of this input to the sloganInput tracked in state to make this a 'controlled' form input */}
-        <input />
-        <button onChange={setSloganInput} value={sloganInput}>Submit</button>
+        <input onChange={(e) => setSloganInput(e.target.value)} required value={sloganInput}/>
+        <button>Submit</button>
       </form>
     </section>
   );
