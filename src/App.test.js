1fd4b51002dea2069a73c5bba79e21b5a2369457
Abriel Cleaver <abrielcleaver@gmail.com>
2022-01-31 19:39:48 -0700

    Update test to check for rendering of welcome message

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome to city message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText(/welcome to/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
